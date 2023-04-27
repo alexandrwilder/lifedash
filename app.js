f446beaf662e9bfd5470e096a29dddc93c16bc59
alexandrwilder <alexander@growhouse.co>
2023-04-27 22:37:27 +0000

    Added updated app.js

--- app.js
+++ app.js
@@ -3,63 +3,83 @@ const mysql = require("mysql");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const session = require('express-session');
+require("dotenv").config();
 
 const app = express();
 
-// Replace with your MySQL credentials
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "alexander_growhouse",
-  password: "i%b2kQexAX&VeHf4!UWM$iXKZ$*X",
-  database: "lifeboard",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.listen(process.env.PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
 
 db.connect((err) => {
   if (err) {
     throw err;
-  } 
+  }
   console.log("MySQL connected...");
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set up session middleware
-app.use(session({
-  secret: "my-secret",
-  resave: false,
-  saveUninitialized: true,
-}));
-
-// Serve index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Define middleware to check if user is authenticated
 const isAuthenticated = (req, res, next) => {
   if (req.session.user || req.headers.authorization) {
-    // If the user is authenticated, allow the request to proceed
     next();
   } else {
-    // If the user is not authenticated, redirect to the login page
     res.redirect('/login');
   }
 };
 
-// Serve dashboard.html only to authenticated users
-// Define the /dashboard route
 app.get('/dashboard', isAuthenticated, (req, res) => {
   res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
 
 // Define the /saveUrls API endpoint
 app.post('/saveUrls', isAuthenticated, (req, res) => {
-  const userId = req.session.user.id;
-  const { jsonUrl, defaultSelection } = req.body;
+  const urls = req.body.urls;
+  const userId = req.session.user.id; // add this line to extract userId from the session
+  const type = req.body.type;
+  const description = req.body.description;
+  if (!userId) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+  const query = 'INSERT INTO user_urls (user_id, url, description, type) VALUES (?, ?, ?, ?)';
+  const values = urls.map(url => [userId, url, description, type]);
+  db.query(query, [values], (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error saving URLs');
+      return;
+    }
+    res.sendStatus(200);
+  });
+});
+
+
+app.post('/saveJsonUrl', isAuthenticated, (req, res) => {
+  const userId = req.session.user.id; // extract userId from the session
+  const jsonUrl = req.body.jsonUrl;
+  const defaultSelection = req.body.defaultSelection;
 
   // First, remove any existing URLs for this user
-  const deleteQuery = "DELETE FROM user_urls WHERE userID = ?";
+  const deleteQuery = "DELETE FROM user_urls WHERE user_id = ?";
   db.query(deleteQuery, [userId], (err) => {
     if (err) {
       console.error('Error during URL deletion:', err);
@@ -68,14 +88,14 @@ app.post('/saveUrls', isAuthenticated, (req, res) => {
     }
 
     // Insert the new JSON URL into the user_urls table
-    const insertQuery = "INSERT INTO user_urls (userID, json_url) VALUES (?, ?)";
+    const insertQuery = "INSERT INTO user_urls (user_id, json_url) VALUES (?, ?)";
     db.query(insertQuery, [userId, jsonUrl], (err) => {
       if (err) {
         console.error('Error during URL insertion:', err);
         res.status(500).json({ error: "Failed to save URLs" });
       } else {
         // Update the user's default selection in the user_settings table
-        const updateQuery = "INSERT INTO user_settings (userID, setting_name, setting_value) VALUES (?, 'default_selection', ?) ON DUPLICATE KEY UPDATE setting_value = ?";
+        const updateQuery = "INSERT INTO user_settings (user_id, setting_name, setting_value) VALUES (?, 'default_selection', ?) ON DUPLICATE KEY UPDATE setting_value = ?";
         db.query(updateQuery, [userId, defaultSelection, defaultSelection], (err) => {
           if (err) {
             console.error('Error during default selection update:', err);
@@ -88,6 +108,39 @@ app.post('/saveUrls', isAuthenticated, (req, res) => {
     });
   });
 });
+
+  // First, remove any existing URLs for this user
+  // First, remove any existing URLs for this user
+const deleteQuery = "DELETE FROM user_urls WHERE user_id = ?";
+db.query(deleteQuery, [userId], (err) => {
+  if (err) {
+    console.error('Error during URL deletion:', err);
+    res.status(500).json({ error: "Failed to save URLs" });
+    return;
+  }
+
+  // Insert the new JSON URL into the user_urls table
+  const insertQuery = "INSERT INTO user_urls (user_id, json_url) VALUES (?, ?)";
+  db.query(insertQuery, [userId, jsonUrl], (err) => {
+    if (err) {
+      console.error('Error during URL insertion:', err);
+      res.status(500).json({ error: "Failed to save URLs" });
+    } else {
+      // Update the user's default selection in the user_settings table
+      const updateQuery = "INSERT INTO user_settings (user_id, setting_name, setting_value) VALUES (?, 'default_selection', ?) ON DUPLICATE KEY UPDATE setting_value = ?";
+      db.query(updateQuery, [userId, defaultSelection, defaultSelection], (err) => {
+        if (err) {
+          console.error('Error during default selection update:', err);
+          res.status(500).json({ error: "Failed to save default selection" });
+        } else {
+          res.status(200).json({ message: "JSON URL and default selection saved successfully" });
+        }
+      });
+    }
+  });
+});
+
+
 
 
 
